2165b28f896ef6d47ede266a661e31876b8b6f4e
Bunnet <phoung.bunnet19@kit.edu.kh>
2021-03-15 17:55:01 +0700

    first commit

---
+++ CovidWebsite/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CovidwebsiteConfig(AppConfig):
+    name = 'CovidWebsite'
---
+++ CovidWebsite/views.py
@@ -0,0 +1,5 @@
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+def index(request):
+    return render(request, 'index.html')
